857dc2df70b111a9f2338c998b0db7fcc8582ce5
Oostrom, Marjolein T <marjolein.oostrom@pnnl.gov>
2023-07-03 00:34:16 -0700

    add viz python

---
+++ visualize_correct.py
@@ -0,0 +1,88 @@
+import os
+import numpy as np
+import cv2
+import os
+from os import listdir, makedirs
+from os.path import join
+from PIL import Image
+import shutil
+import sys
+import logging
+
+
+def write_folder_stack(vol, path):
+    if os.path.exists(path):
+        print("Overwriting " + path)
+        shutil.rmtree(path)
+
+    makedirs(path)
+
+    for i in range(vol.shape[0]):
+        fname = os.path.join(path, "slice" + str(i).zfill(5) + ".tiff")
+        cv2.imwrite(fname, vol[i])
+    # dim_offset = 14
+    # for i in range(vol.shape[0]):
+    #     if i > 13 and i < vol.shape[0] -13:
+    #         fname = os.path.join(path, "slice" + str(i).zfill(5) + ".tiff")
+    #         cv2.imwrite(fname, vol[i][dim_offset:-dim_offset,dim_offset:-dim_offset])
+
+
+def read_tiff_stack(path):
+    img = Image.open(path)
+    images = []
+    for i in range(img.n_frames):
+        img.seek(i)
+        slice = np.array(img)
+
+        images.append(slice)
+
+    return np.array(images)
+
+
+def get_dir(path):
+    tiffs = [os.path.join(path, f) for f in os.listdir(path) if f[0] != "."]
+
+    return sorted(tiffs)
+
+
+# 74 for testing
+#
+input_folder = "data/testing/testing-original/labels"
+file_names = get_dir(
+    "/Users/oost464/Library/CloudStorage/OneDrive-PNNL/Desktop/projects/brain_ohsu/data/testing/testing-original/labels/"
+)
+vol = read_tiff_stack(file_names[0])
+write_folder_stack(vol, os.path.join(input_folder, f"slices_truth_labels"))
+
+pred_image_array = np.array(
+    Image.open(
+        "data/testing/seg-_overlap_1.2_best_weights_checkpoint_all_changes_2X_20_aug_rot_normal_False.hdf5_guass_False_testing-original/seg-slice00074.tiff"
+    )
+)
+# /data/model-weights/best_weights_checkpoint_all_changes_2X_20_aug_rot_normal_False.hdf5
+print(pred_image_array[50,50])
+truth_array = np.array(
+    Image.open("data/testing/testing-original/labels/slices_truth_labels/slice00074.tiff")
+)
+dim_offset = 14
+truth_array = truth_array[dim_offset:-dim_offset,dim_offset:-dim_offset]
+pred_image_array= pred_image_array[dim_offset:-dim_offset,dim_offset:-dim_offset]
+print(np.unique(truth_array, return_counts=True))
+
+imgray_pred = np.empty((truth_array.shape[0], truth_array.shape[1], 3))
+tp_locs = np.where((pred_image_array >= 0.5) & (truth_array == 2))
+print(len(tp_locs[0]))
+imgray_pred[tp_locs[0], tp_locs[1], :] = (0, 0, 255)  # blue
+tn_locs = np.where((pred_image_array < 0.5) & (truth_array == 2))
+print(len(tn_locs[0]))
+imgray_pred[tn_locs[0], tn_locs[1], :] = (255, 255, 0)  # yellow false negative
+fn_locs = np.where((pred_image_array >= 0.5) & (truth_array != 2))
+print(len(fn_locs[0]))
+# false positive
+imgray_pred[fn_locs[0], fn_locs[1], :] = (255, 0, 0)  # red false
+
+
+im = Image.fromarray(imgray_pred.astype(np.uint8))
+im.save(
+    "seg-_overlap_1.2_best_weights_checkpoint_all_changes_2X_20_aug_rot_normal_False.hdf5_guass_False_testing-original_fp_fn.tif"
+)
